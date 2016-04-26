384ae315f9578cdac77e3535a3236baebe0a412d
Dion <Dion.Fulwood@gmail.com>
2016-04-26 15:21:31 -0700

    Finished post and get requests

--- app/routes.js
+++ app/routes.js
@@ -1,1 +1,42 @@
 
+module.exports = function(app){
+
+//get for business model  to retrieve information within Business database
+app.get('/api/business', function(req, res){
+  Business.find({}, function(err, business){
+  if(err){ 
+    res.send(err);
+  };
+  res.json(business);
+    
+  });
+});
+
+app.post('/api/business', function(req, res){
+  //create new instance of the business model and add properties to new instance
+  var newBusiness = new Business();
+  newBusiness.name: req.body.name,
+  newBusiness.address: req.body.address,
+  newBusiness.phoneNumber: req.body.phoneNumber,
+  newBusiness.email: req.body.email,
+  newBusinesss.description: req.body.description,
+  newBusiness.picture: req.body.picture,
+  newBusiness.menu: req.body.menu,
+  newBusiness.link: req.body.link
+
+  //save the newly created model into the database
+  newBusiness.save(function(err, newBusiness){
+    if (err) {
+      //log error if one exists 
+      console.log(err)
+      res.send({errorMessage: err});
+     }else{
+      res.json(req.body);
+     }
+  })
+
+
+});
+
+}
+
