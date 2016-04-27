8e015719d7db7ac3362af9931360786155c6bae4
selfresonator <selfresonator@gmail.com>
2016-04-27 12:01:00 -0700

    Readying commit for merge

--- public/appRoutes.js
+++ public/appRoutes.js
@@ -3,14 +3,14 @@ angular.module('appRoutes', [])
 .config(['$routeProvider',
   function($routeProvider) {
 
-    These
+    // These
     $routeProvider
       .when('/', {
         templateUrl: 'views/viewIndex.html',
         controller: 'MainController'
       })
       .when('/register', {
-        templateUrl: 'views/',
+        templateUrl: 'views/busRegistration.html',
         controller: 'RegController'
       })
       .when('/profile', {
