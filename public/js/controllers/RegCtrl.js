06775792360636547a2b9c4e509bd72d8bd93970
selfresonator <selfresonator@gmail.com>
2016-04-29 13:31:10 -0700

    Profile Registration form now linking to profile page.

--- public/js/controllers/RegCtrl.js
+++ public/js/controllers/RegCtrl.js
@@ -1,6 +1,6 @@
 angular.module('RegCtrl', [])
 
-.controller('RegController', function($scope, RegFactory) {
+.controller('RegController', function($scope, $location, RegFactory) {
   //changed below to $scope.data -- $scope.registerBusiness function couldn't be invoked before change
   $scope.data = {
     company: '',
@@ -12,14 +12,17 @@ angular.module('RegCtrl', [])
     url: ''
   };
 
-  $scope.registerBusiness = function(busName, busSpecialty, busAddress, busPhone, busEmail, busWebsiteLink, busPic, busMenu, busDescription) {
+  $scope.registerBusiness = function(busName, busSpecialty, busAddress, busPhone, busEmail, busWebsiteLink, busPic, busMenu, busDescription, $window) {
     console.log('inside RegCtrl:', busName, busSpecialty, busAddress, busPhone, busEmail, busWebsiteLink, busPic, busMenu, busDescription);
-    RegFactory.addBusiness(busName, busSpecialty, busAddress, busPhone, busEmail, busWebsiteLink, busPic, busMenu, busDescription);
-  }
+    RegFactory.addBusiness(busName, busSpecialty, busAddress, busPhone, busEmail, busWebsiteLink, busPic, busMenu, busDescription)
+      .then(function() {
+        $location.path('/profile');
+      });
+  };
 
   $scope.registerUser = function(userName, userPassword,fullName, userAddress, userPhone, userEmail, userPic) {
     console.log('inside userCtrl:', userName, userPassword,fullName, userAddress, userPhone, userEmail, userPic);
     RegFactory.addUser(userName, userPassword,fullName, userAddress, userPhone, userEmail, userPic);
-  }
+  };
 
 });
