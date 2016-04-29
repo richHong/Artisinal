6ba4322885190fd79cd02ee07db3cb1dc74d5d92
Aralya Phinith <aphinith84@gmail.com>
2016-04-29 00:57:01 -0700

    UserCtrl created - dropdown menu selection now responsive with function call

---
+++ public/js/controllers/UserCtrl.js
@@ -0,0 +1,13 @@
+angular.module('UserCtrl', [])
+
+.controller('UserSelectController', function($scope) {
+  //list out all of our specialty types of food in the below array -- this will populate in our dropdown selection for our user_BusDirectory.html view
+  $scope.listOfTypes = ['Type 1', 'Type 2', 'Type 3', 'Type 4', 'Type 5', 'Type 6', 'Type 7', 'Type 8'];
+  //invoke function to call a GET request to get business with that type of specialty
+  $scope.getBusiness = function(){
+    $scope.businessType = $scope.selectedType;
+    console.log('You selected: ', $scope.selectedType);
+  };
+
+
+})
