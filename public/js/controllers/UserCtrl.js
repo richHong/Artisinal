0b56ebe45d5dd7716360222d364fcf5b114467ca
selfresonator <selfresonator@gmail.com>
2016-04-30 22:10:58 -0700

    Linking Business pages to business directory.

--- public/js/controllers/UserCtrl.js
+++ public/js/controllers/UserCtrl.js
@@ -6,20 +6,18 @@ angular.module('UserCtrl', [])
   $scope.listOfTypes = ['Type 1', 'Type 2', 'Type 3', 'Type 4', 'Type 5', 'Type 6', 'Type 7', 'Type 8'];
   //invoke function to call a GET request to get business with that type of specialty
 
-  $scope.getBusiness = function(){
-    
-  console.log('You selected: ', $scope.selectedType);
-  queryFactory.queryType($scope.selectedType)
-  .then(function(res) {
-    console.log('this is res:', res);
-    $scope.data.list = res
-  });
-  // console.log("What is $scope.data.list", $scope.data.list);
-
-  }
+  $scope.getBusiness = function() {
+    console.log('You selected: ', $scope.selectedType);
+    queryFactory.queryType($scope.selectedType)
+      .then(function(res) {
+        console.log('this is res:', res);
+        $scope.data.list = res
+      });
+    // console.log("What is $scope.data.list", $scope.data.list);
+  };
 
   $scope.loginUser = function() {
     alert('login function called!');
   };
 
-})
+});
