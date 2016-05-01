abace336fc1fecf270d9efd50f1249f136036151
selfresonator <selfresonator@gmail.com>
2016-04-30 19:43:56 -0700

    Minor changes for site navigation

--- public/js/controllers/RegCtrl.js
+++ public/js/controllers/RegCtrl.js
@@ -11,13 +11,15 @@ angular.module('RegCtrl', [])
     description: '',
     url: ''
   };
-  $scope.registerBusiness = function(busName, busSpecialty, busAddress, busPhone, busEmail, busWebsiteLink, busPic, busMenu, busDescription, $window) {
+  
+  $scope.registerBusiness = function(busName, busSpecialty, busAddress, busPhone, busEmail, busWebsiteLink, busPic, busMenu, busDescription) {
     console.log('inside RegCtrl:', busName, busSpecialty, busAddress, busPhone, busEmail, busWebsiteLink, busPic, busMenu, busDescription);
     RegFactory.addBusiness(busName, busSpecialty, busAddress, busPhone, busEmail, busWebsiteLink, busPic, busMenu, busDescription)
     .then(function() {
       $location.path('/profile');
     });
   };
+
   $scope.registerUser = function(userName, userPassword,fullName, userAddress, userPhone, userEmail, userPic) {
     console.log('inside userCtrl:', userName, userPassword,fullName, userAddress, userPhone, userEmail, userPic);
     RegFactory.addUser(userName, userPassword,fullName, userAddress, userPhone, userEmail, userPic);
