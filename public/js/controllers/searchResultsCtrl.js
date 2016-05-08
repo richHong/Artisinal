f652b56d9f696a93ae2ebb13aed735e16d17da07
Andy Nixon <andielane@yahoo.com>
2016-05-07 17:03:10 -0700

    Fixing the double database call in the Results Page.

--- public/js/controllers/searchResultsCtrl.js
+++ public/js/controllers/searchResultsCtrl.js
@@ -2,9 +2,10 @@ angular.module('searchResultsCtrl', [])
 
 .controller('searchResultsCtrl', function($scope, $route ,QueryFactory) {
   $scope.getResults = function(){
-    console.log("Why am I being called twice?")
+    console.log("Why am I being called twice?", searchQArr);
     QueryFactory.queryType(searchQArr).then(function(){
+      console.log("AM I BEING CALLED TWICE")
     })
   };
-  $scope.getResults();
+  // $scope.getResults();
 });
